aa84007af0663b178da5f7e5d38cea8165e634b6
Dominik Haid <info@dominikhaid.de>
2021-11-02 00:54:04 +0100

    remove lxde as default install

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -10,14 +10,12 @@ user=('USER SETUP' user)
 configs=('UPDATING CONFIG' configs)   
 system=('INSTALL BASE SYSTEM' system)   
 libs=('INSTALL LIBS' libs)   
-desktop=('INSTALL LXDE' desktop)   
 node=('INSTALL NODE' node)   
     
 PRE+=(
     user[@]
     system[@]
     libs[@]
-    desktop[@]
     node[@]
     configs[@]
 )
