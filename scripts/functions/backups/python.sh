12f0ebc38ccac4f1ebec69960221b9f78b3c2c12
Dominik Haid <info@dominikhaid.de>
2021-11-02 01:03:54 +0100

    remove virtuual python env

--- scripts/functions/backups/python.sh
+++ scripts/functions/backups/python.sh
@@ -8,16 +8,8 @@ source $SCRIPTPATH/scripts/setIndicator.sh
 python() {
 	build() {
 		sudo -i -u $USER_NAME <<EOF
-    pip3 install 'xcffib>=0.5.0'
-    pip3 install --no-deps --ignore-installed cairocffi
-    pip3 install dbus-next
-    pip3 install --upgrade psutil
-    pip3 install qtile
     pip3 install rofimoji
 
-    if ! [ -d $USER_HOME/dev/qtile ]; then
-        cd $USER_HOME && git clone git://github.com/qtile/qtile.git $USER_HOME/dev/qtile
-    fi
 
     if ! [ -d $USER_HOME/dev/virtualenvs/debugpy ]; then
           python3 -m venv $USER_HOME/dev/virtualenvs/debugpy 
