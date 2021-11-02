e62913b3f0862588d5f9ad6790b4b7c58e0ee17d
Dominik Haid <info@dominikhaid.de>
2021-11-02 01:11:17 +0100

    remove virtual python env
    fix

--- scripts/functions/backups/python.sh
+++ scripts/functions/backups/python.sh
@@ -6,14 +6,21 @@
 source $SCRIPTPATH/scripts/setIndicator.sh
 
 python() {
+
+	apt install -y python-pip
+	apt install -y pytho3-pip
+
 	build() {
 		sudo -i -u $USER_NAME <<EOF
+    pip3 install 'xcffib>=0.5.0'
+    pip3 install --no-deps --ignore-installed cairocffi
+    pip3 install dbus-next
+    pip3 install --upgrade psutil
+    pip3 install qtile
     pip3 install rofimoji
 
-
-    if ! [ -d $USER_HOME/dev/virtualenvs/debugpy ]; then
-          python3 -m venv $USER_HOME/dev/virtualenvs/debugpy 
-          $USER_HOME/dev/virtualenvs/debugpy/bin/python3 -m pip install debugpy 
+    if ! [ -d $USER_HOME/qtile ]; then
+        cd $USER_HOME && git clone git://github.com/qtile/qtile.git $USER_HOME/dev/qtile
     fi
 EOF
 
@@ -21,8 +28,8 @@ EOF
     pip3 install ueberzug
     pip3 install --user pywal
  
-if [ -d "$HOME/Bilder/wallpaper" ]; then
-    wal -n -i "$HOME/Bilder/wallpaper"
+if [ -d "$USER_HOME/Bilder/wallpaper" ]; then
+    wal -n -i "$USER_HOME/Bilder/wallpaper"
  fi
 EOF
 	}
@@ -31,6 +38,10 @@ EOF
 		$LOGPATH/err/python.log &
 
 	setIndicator "Python" ${WORKINGICONS[2]} $!
+
+	if [ -f "$USER_HOME/.local/bin/wal" ]; then
+		ln -s $USER_HOME/.local/bin/wal $USER_HOME/bin/wal
+	fi
 }
 
 if [[ ${1} == "--debug" ]]; then
